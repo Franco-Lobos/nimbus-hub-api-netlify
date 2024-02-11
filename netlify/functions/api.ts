2a18bff0da1d0f4505d277c9213236b7514a443a
Franco <francolobos3@gmail.com>
2024-02-11 23:54:00 +0100

    server.js/function

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,18 +1,3 @@
-// // YOUR_BASE_DIRECTORY/netlify/functions/api.ts
-
-// import express, { Router } from "express";
-// import serverless from "serverless-http";
-
-// const api = express();
-
-// const router = Router();
-// router.get("/hello", (req, res) => res.send("Hello World!"));
-
-// api.use("/api/", router);
-
-// export const handler = serverless(api);
-
-
 import serverless from "serverless-http";
 import { createRequestHandler } from "@remix-run/express";
 import {  broadcastDevReady } from "@remix-run/node";
@@ -29,17 +14,13 @@ import router from '../../dbConnection/router/index.js';
 import http from 'node:http';
 
 // notice that the result of `remix build` is "just a module"
-import * as build from "@remix-run/dev/server-build";
 import { config as dotenvConfig } from 'dotenv';
 import { manageNotFound } from "dbConnection/middlewares/manageNotFound.js";
-
 
 dotenvConfig();
 
 const api = express();
 const mogoUrl :string = process.env.MONGO_URL!;
-const expressPort = 8080;
-
 
 api.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 
@@ -61,24 +42,9 @@ mongoose.connection.on('disconnected', function () {
 });
 
 
-// Router
-// const router2 = Router();
-// router2.get("/hello", (req, res) => res.send("Hello World!"));
-// api.use("/api/", router());
-
 api.use('/api/', router());
 
 // Error-handling middleware
 api.use(manageNotFound);
-
-// const server = http.createServer(api);
-
-// // Start the server
-// server.listen(expressPort, () => {
-//   if (process.env.NODE_ENV === "development") {
-//     // broadcastDevReady({ build: serverBuild });
-//   }
-//   console.log(`Express server listening on port ${expressPort}`);
-// });
 
 export const handler = serverless(api);
